a48c07c75badf430ffc0d73803ab7256934fa2a1
zamgi <denisov72@mail.ru>
2023-04-13 15:08:34 +0300

    ...

--- LanguageDetector.WebService/wwwroot/js/main.js
+++ LanguageDetector.WebService/wwwroot/js/main.js
@@ -5,12 +5,8 @@ $(document).ready(function () {
 'У жывёл найбольш вылучаюць спангіяформную энцэфалапатыю буйной рагатай жывёлы т.зв. шаленства кароў ад мяса якіх адбываецца заражэнне людзей скрыты перыяд у людзей - гадоў.';
 
     var textOnChange = function () {
-        var _len = $("#text").val().length; 
-        var len = _len.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-        var $textLength = $("#textLength");
-        $textLength.html("length of text: " + len + " characters");
-        if (MAX_INPUTTEXT_LENGTH < _len) $textLength.addClass("max-inputtext-length");
-        else                             $textLength.removeClass("max-inputtext-length");
+        let len = $('#text').val().length, len_txt = len.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+        $('#textLength').toggleClass('max-inputtext-length', MAX_INPUTTEXT_LENGTH < len).html('length of text: ' + len_txt + ' characters');
     };
     var getText = function ($text) {
         var text = trim_text($text.val().toString());
@@ -44,7 +40,7 @@ $(document).ready(function () {
     })();
     $('#resetText2Default').click(function () {
         $("#text").val('');
-        setTimeout(function () { $("#text").val(DEFAULT_TEXT).focus(); }, 100);
+        setTimeout(() => $("#text").val(DEFAULT_TEXT).focus(), 100);
     });
 
     $('#processButton').click(function () {
